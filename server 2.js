5dbd64b1d477c1005503f5aadfdc1e5f32d9c7a3
choisangwon <choisangwon2170@daum.net>
2020-07-12 17:23:01 +0900

    req,res check

--- server 2.js
+++ server 2.js
@@ -1,16 +1,6 @@
 const express = require('express');
 const app = express();
 
-
-app.use((req, res) => {
-    res.json({
-        message : "data get"
-    });
-});
-
-
-
-
-const port = 5000;
+const port = 5000
 
 app.listen(port, console.log("server started"));
